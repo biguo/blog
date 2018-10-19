db68a81b5f565db4f3f5aadab4c4d7f3edaf4cc6
xiaodan <624136772@qq.com>
2018-10-19 14:30:08 +0800

    store and article

---
+++ database/migrations/2018_10_19_031629_add_types_to_stores_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTypesToStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('stores', function (Blueprint $table) {
+            $table->integer('types')->default(0)->comment('类型');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('stores', function (Blueprint $table) {
+            //
+        });
+    }
+}
